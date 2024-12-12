b8a8f760a7abb4bb823e0b742e05fea49cf1ff21
Karan-1208 <mkarandeep.1208@gmail.com>
2024-12-12 14:08:24 +0530

    completed milestone 5

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Login from './components/authorization/login.jsx'
-
+import Signup from './components/authorization/signup.jsx'
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div><Login /></div>
-  )
+    // <div><Signup /></div>
+    <Routes>
+        <Route path="/" />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+  );
 }
 
 export default App;
