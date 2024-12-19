5f10c7609a96e3f9d4b66d0360d6f6d1aa27803f
Karan-1208 <mkarandeep.1208@gmail.com>
2024-12-19 15:27:05 +0530

    completed milestone9

--- Frontend/frontend/src/Pages/HomePage.jsx
+++ Frontend/frontend/src/Pages/HomePage.jsx
@@ -10,14 +10,18 @@ function HomePage() {
 
   return (
     <>
-      <h1 className="text-center">'Home Page fro Follow along';</h1>
-
+      <h1 className="text-center">Home Page for Follow Along</h1>
       <div className="grid grid-cols-3">
         {data.map((ele, index) => {
           return (
-            <div style={{ margin: 'auto' }}>
-              <Card title={ele.title} Index={index} />;
-            </div>
+            <>
+              <div style={{ margin: 'auto' }}>
+                <Card title={ele.title} Index={index} />
+              </div>
+              <div style={{ margin: 'auto' }} className="border border-white">
+                <Card title={ele.title} Index={index} />
+              </div>
+            </>
           );
         })}
       </div>
